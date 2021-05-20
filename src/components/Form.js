c4d23b537e5f95d25d2298933466a938da98be79
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-05-20 21:37:09 +1000

    style form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,13 @@ import "./../styles/Form.scss";
 const Form = () => {
   return (
       <>
-      <form action=""></form>
+        <form  class="flex-form">
+            <label for="from">
+                <i class="ion-location"></i>
+            </label>
+            <input type="search" placeholder="Where do you want to go?" />
+            <input type="submit" value="Go" />
+        </form>
       </>
   )
 };
